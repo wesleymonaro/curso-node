2de4511c42f15f3e8f4578804d18adc3e2ff9a62
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-13 21:32:18 -0300

    Clusters

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
-var fs = require('fs'),
+var cluster = require('cluster'),
     http = require('http'),
-    url = require('http'),
-    path = require('path');
+    numCPUs = require('os').cpus().length;
 
-http.createServer((req, res) => {
-    if (req.url !== '/movie.mp4') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<video src="http://localhost:3000/movie.mp4" controls></video>');
-
-    } else {
-        var file = path.resolve(__dirname, "movie.mp4");
-        var range = req.headers.range;
-        var positions = range.replace(/bytes=/, "").split('-');
-        var start = parseInt(positions[0], 10);
-
-        fs.stat(file, (err, stats) => {
-            var total = stats.size;
-            var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-            var chunksize = (end - start) + 1;
-
-            res.writeHead(200, {
-                "Content-Range": "bytes " + start + "-" + end + "/" + total,
-                "Accept-Ranges": "bytes",
-                "Content-Length": chunksize,
-                "Content-Type": "video/mp4"
-            })
-
-            var stream = fs.createReadStream(file, { start, end })
-                .on('open', () => {
-                    stream.pipe(res);
-                })
-                .on('error', () => {
-                    res.end(err);
-                })
-        })
+if (cluster.isMaster) {
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
     }
-}).listen(3000);
+
+    cluster.on('online', (worker) => {
+        console.log(`worker ${worker.process.pid} is online`);
+    })
+
+    cluster.on('listening', (address) => {
+        console.log(`worker is listening`);
+    })
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    })
+} else {
+    http.createServer((req, res) => {
+        res.writeHead(200);
+        res.end(`process ${process.pid} says hello!`)
+    }).listen(8000)
+}
